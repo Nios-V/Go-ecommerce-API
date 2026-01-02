432de63498c8dee8456f9b9aa5a040cef8056a54
Nicolás Cáceres <niosv.1104@gmail.com>
2026-01-01 23:07:52 -0300

    feat: add repositories to initialize

--- internal/handler/registry.go
+++ internal/handler/registry.go
@@ -13,7 +13,16 @@ type Registry struct {
 
 func NewRegistry(db *gorm.DB) *Registry {
 	// Initialize repositories
+	addressRepo := repository.NewAddressRepository(db)
+	cartItemRepo := repository.NewCartItemRepository(db)
+	cartRepo := repository.NewCartRepository(db)
 	categoryRepo := repository.NewCategoryRepository(db)
+	orderItemRepo := repository.NewOrderItemRepository(db)
+	orderRepo := repository.NewOrderRepository(db)
+	paymentRepo := repository.NewPaymentRepository(db)
+	productRepo := repository.NewProductRepository(db)
+	userRepo := repository.NewUserRepository(db)
+	roleRepo := repository.NewRoleRepository(db)
 
 	// Initialize services
 	categoryService := service.NewCategoryService(db, categoryRepo)
